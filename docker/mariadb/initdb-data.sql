60d092731941f2026f3840ae4f2b112be14b75a4
Eliecer García Rey <egarciarey1981@gmail.com>
2022-12-03 17:13:13 +0100

    mariadb

---
+++ docker/mariadb/initdb-data.sql
@@ -0,0 +1,7 @@
+USE bookstore;
+
+INSERT INTO Genres (GenreId, GenreName) VALUES
+('196ba51e-afb9-4f9f-a8df-4dfa07f5935f', 'Adventure'),
+('30579b36-571f-4275-85d2-6f9535962781', 'Fantasy'),
+('a8d0bca8-c65e-40bb-a7a8-184d87bd58b3', 'Horror'),
+('0dd82a68-24b9-4eec-84e9-5e6fba4dfdf2', 'Crime');
---
+++ docker/mariadb/initdb-schema.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE bookstore;
+
+USE bookstore;
+
+CREATE TABLE Genres (
+    GenreId varchar(255),
+    GenreName varchar(255),
+    PRIMARY KEY (GenreId)
+);
