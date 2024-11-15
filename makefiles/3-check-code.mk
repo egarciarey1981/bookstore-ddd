e3354e80f92f4f34430435917ba57ccdf9425408
Eliecer García Rey <egarciarey1981@gmail.com>
2024-11-15 20:32:59 +0100

    phpcs and phpstan file config

--- makefiles/3-check-code.mk
+++ makefiles/3-check-code.mk
@@ -1,8 +1,8 @@
 phpstan: ## Run PHPStan
-	docker compose exec api vendor/bin/phpstan analyse --level max src app || true
+	docker compose exec api vendor/bin/phpstan analyse || true
 
 phpcs: ## Run PHP Code Sniffer
-	docker compose exec api vendor/bin/phpcs --standard=PSR12 src || true
+	docker compose exec api vendor/bin/phpcs || true
 
 phpcbf: ## Run PHP Code Beautifier and Fixer
-	docker compose exec api vendor/bin/phpcbf --standard=PSR12 src || true
+	docker compose exec api vendor/bin/phpcbf || true
